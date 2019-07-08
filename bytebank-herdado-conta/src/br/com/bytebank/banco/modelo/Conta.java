f689fac462e97b9d628381d8c0eccef3927867d2
Everton Paiva <evertonpaiva@gmail.com>
2019-07-08 16:08:38 -0300

    Sobrescrevendo o método toString().

--- bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -90,5 +90,10 @@ public abstract class Conta {
     public static int getTotal(){
         return Conta.total;
     }
+    
+	@Override
+	public String toString() {
+		return "Numero: " + this.getNumero() + ", Agencia: " + this.agencia;
+	}
 
 }
